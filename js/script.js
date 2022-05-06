be63e22921370958b65b4ef0b46c6f8fe600a1fd
mohamedslima <hammaslima@gmail.com>
2022-05-06 16:00:45 +0200

    added function previous slide and started next

--- js/script.js
+++ js/script.js
@@ -24,39 +24,51 @@
 
 Vue.config.devtools = true;
 
-var app = new Vue(
+Vue.config.devtools = true;
+const app = new Vue (
     {
         el: '#root',
         data: {
-            slide: [
+            slides: [
                 {
-                    image: 'img/01.jpg',
-                    title: 'Svezia',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                    image:'img/01.jpg',
+                    title:'Svezia',
+                    text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
                 },
                 {
-                    image: 'img/01.jpg',
-                    title: 'Svezia',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                    image:'img/02.jpg',
+                    title:'Svizzera',
+                    text:'Lorem ipsum'
                 },
                 {
-                    image: 'img/01.jpg',
-                    title: 'Svezia',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                    image:'img/03.jpg',
+                    title:'Gran Bretagna',
+                    text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
                 },
                 {
-                    image: 'img/01.jpg',
-                    title: 'Svezia',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                    image:'img/04.jpg',
+                    title:'Germania',
+                    text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
                 },
                 {
-                    image: 'img/01.jpg',
-                    title: 'Svezia',
-                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                }
+                    image:'img/05.jpg',
+                    title:'Paradise',
+                    text:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                }                
             ],
-        } 
-    },
+            currentSlide: 0                                
+        },
 
-    
+        methods: {
+            // Slide precedente
+            prevSlide(){
+                this.currentSlide --; // precedente
+
+                // se sono alla prima, mi posiziono sull'ultima
+                if(this.currentSlide < 0){
+                    this.currentSlide = this.slides.length - 1; // ultima
+                }
+            },
+        },
+    }
 )
